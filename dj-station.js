d038dc9fbfb9e77702603bf7701715bef8a9c8a5
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-11-02 13:55:20 -0700

    added html and css for song

--- dj-station.js
+++ dj-station.js
@@ -5,11 +5,33 @@ swapButton.addEventListener("click", swapDecks);
 // get variables for status
 const aStatus = document.getElementById("a-status");
 const bStatus = document.getElementById("b-status");
+// get status bar boxes
+const aStatusBox = document.getElementById("a-status-box");
+const bStatusBox = document.getElementById("b-status-box");
+// elements for body background and dj image
+const djImage = document.getElementById('dj-image');
 
+// global variables for cue deck, start at B
+let bInCue = true;
+let aInCue = false;
 
+/** Swap status text and change order of status bars */
 function swapDecks() {
   let prevAstatus = aStatus.innerText;
   let prevBstatus = bStatus.innerText;
   aStatus.innerText = prevBstatus;
   bStatus.innerText = prevAstatus;
+  // move a from cue to play
+  if (prevAstatus == "In the cue:"){
+    aStatusBox.after(bStatusBox);
+    djImage.style.transform = 'scaleX(1)'
+  }
+  // move b from cue to play
+  else {
+    bStatusBox.after(aStatusBox);
+    djImage.style.transform = 'scaleX(-1)';
+  }
+  // flip cue statuses
+  aInCue = !aInCue;
+  bInCue = !bInCue;
 }
