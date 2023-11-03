ae66589497e6df76001da03e2a357414fa86ea5b
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-11-02 18:04:20 -0700

    working on form submit

--- dj-station.js
+++ dj-station.js
@@ -2,6 +2,16 @@
 const swapButton = document.getElementById("swap-button");
 swapButton.addEventListener("click", swapDecks);
 
+// add event listener for song entry
+const form = document.querySelector("form");
+form.addEventListener("submit", addNextSong);
+// entries for song
+const songSelect = document.getElementById("song-select");
+const artistSelect = document.getElementById("artist-select");
+const bpmSelect = document.getElementById("bpm-select");
+const keySelect = document.getElementById("key-select");
+const addSongResult = document.getElementById("add-song-result");
+
 // get variables for status
 const aStatus = document.getElementById("a-status");
 const bStatus = document.getElementById("b-status");
@@ -35,3 +45,19 @@ function swapDecks() {
   aInCue = !aInCue;
   bInCue = !bInCue;
 }
+
+/** Add song using form entry */
+function addNextSong(event) {
+  event.preventDefault();
+  console.log('adding song');
+  // if no song then don't proceed
+  if (songSelect.value.length == 0){
+    console.log('no song name entered');
+    addSongResult.innerText = "No song provided."
+    return;
+  }
+  for (let item of [songSelect, artistSelect, bpmSelect, keySelect]){
+    console.log(item.value);
+  }
+}
+
