6418379acb4633253fed20dbc7b1fafc85a40a82
Nick Monteleone <nicholasmonteleone25@gmail.com>
2024-01-15 10:12:53 -0800

    minor html updates

--- dj-station.js
+++ dj-station.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // add event listener for swap button
 const swapButton = document.getElementById("swap-button");
 swapButton.addEventListener("click", swapDecks);
@@ -5,6 +7,7 @@ swapButton.addEventListener("click", swapDecks);
 // add event listener for song entry
 const form = document.querySelector("form");
 form.addEventListener("submit", addNextSong);
+
 // entries for song
 const songSelect = document.getElementById("song-select");
 const artistSelect = document.getElementById("artist-select");
@@ -15,9 +18,11 @@ const addSongResult = document.getElementById("add-song-result");
 // get variables for status
 const aStatus = document.getElementById("a-status");
 const bStatus = document.getElementById("b-status");
+
 // get status bar boxes
 const aStatusBox = document.getElementById("a-status-box");
 const bStatusBox = document.getElementById("b-status-box");
+
 // elements for body background and dj image
 const djImage = document.getElementById('dj-image');
 
@@ -25,11 +30,13 @@ const djImage = document.getElementById('dj-image');
 let inCue = 'b'
 
 /** Swap status text and change order of status bars */
+
 function swapDecks() {
   let prevAstatus = aStatus.innerText;
   let prevBstatus = bStatus.innerText;
   aStatus.innerText = prevBstatus;
   bStatus.innerText = prevAstatus;
+
   // move a from cue to play
   if (prevAstatus == "In the cue:"){
     aStatusBox.after(bStatusBox);
@@ -45,18 +52,26 @@ function swapDecks() {
 }
 
 /** Add song using form entry */
+
 function addNextSong(event) {
   event.preventDefault();
+
   // update each field with form input value
   const idPrefixes = ['song', 'artist', 'bpm', 'key'];
-  const formInputs = [songSelect.value, artistSelect.value,
-    bpmSelect.value, keySelect.value];
+  const formInputs = [
+    songSelect.value,
+    artistSelect.value,
+    bpmSelect.value,
+    keySelect.value
+  ];
+
   for (let i=0; i < formInputs.length; i++){
     let elementToUpdate = document.getElementById(`${idPrefixes[i]}-${inCue}`);
     if (formInputs[i].length == 0) formInputs[i] = '-';
     elementToUpdate.innerText = formInputs[i];
   }
   addSongResult.innerText = `Song added to Deck ${inCue.toUpperCase()}`
+
   // reset form
   form.reset()
 }
